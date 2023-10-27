ecf45ff3376c82ec4ef00a2aab532c9ea1d8f4d0
xiaomao05 <anacropia@163.com>
2023-10-27 18:22:45 -0500

    planet version 2
    planet version 2

--- PlanetBarren2/planet_forest_2.gd
+++ PlanetBarren2/planet_forest_2.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var mass = 1000
-var vegetation = false
+var vegetation = true
 
 var land = false	#whether ship lands on the planet
 var crush = false	#whether ship crushes
@@ -19,15 +19,10 @@ func _process(delta):
 	pass
 
 
-func _on_planet_barren_area_body_entered(body):
+func _on_plante_forest_area_body_entered(body):
 	land = true
 	if body.speed > crushSpeed:		# Speed
 		crush = true
 	if fuel: 
 		pass
-		
 
-
-
-func _on_planet_barren_area_body_exited(body):
-	land = false
