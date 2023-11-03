f620f689d2ba6b38efe62ba8c7ba3d935ea5a4c6
xiaomao05 <144844682+xiaomao05@users.noreply.github.com>
2023-11-03 00:09:56 -0500

    restore project

---
+++ Project/PlanetBarren2/planet_forest_2.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var mass = 1000
+var vegetation = true
+
+var land = false	#whether ship lands on the planet
+var crush = false	#whether ship crushes
+var fuel = false
+var crushSpeed = 300
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_plante_forest_area_body_entered(body):
+	land = true
+	if body.speed > crushSpeed:		# Speed
+		crush = true
+	if fuel: 
+		pass
+
