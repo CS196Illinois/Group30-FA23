3d2dde0be03f604ec925c5d7ad555e2524241961
xiaomao05 <anacropia@163.com>
2023-11-21 01:26:51 -0600

    practicing with a tutorial: https://www.youtube.com/watch?v=WEt2JHEe-do

---
+++ Project/LearnTutorial/AnimatedSprite2D.gd
@@ -0,0 +1,25 @@
+class_name LearnTutorial extends AnimatedSprite2D
+
+@export var speed = 400.0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Vector2.ZERO
+	if Input.is_action_pressed("moveRight"):
+		direction.x += 1
+	if Input.is_action_pressed("moveLeft"):
+		direction.x -= 1
+	if Input.is_action_pressed("moveUp"):
+		direction.y -= 1
+	if Input.is_action_pressed("moveDown"):
+		direction.y += 1
+	
+	if direction.length() > 1:
+		direction = direction.normalized()
+	
+	position += direction * speed * delta
+	
