45665ee2b8a42f13cc560d2fef3d5c94f0643c59
xiaomao05 <anacropia@163.com>
2023-11-21 18:28:17 -0600

    (practicing) add random enermies

--- Project/LearnTutorial/planet_barren.gd
+++ Project/LearnTutorial/planet_barren.gd
@@ -6,8 +6,9 @@ class_name PlanetBarren extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	
-
+	$AnimatedSprite2D.playing = true
+	var PlanetBarren_types = $AnimatedSprite2D.frames.get_animation_names() 
+	$AnimatedSprite2D.animation = PlanetBarren_types[0]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
