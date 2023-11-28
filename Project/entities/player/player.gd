42539fcf909bab4a518fe48c3dccbf48919fe949
adam-younes <samakmashwee@gmail.com>
2023-11-27 18:47:34 -0600

    Player and general structure

--- Project/entities/player/player.gd
+++ Project/entities/player/player.gd
@@ -5,23 +5,29 @@ extends CharacterBody2D
 
 # Player movement constants
 const MASS = 10
-const SPEED = 500
+const SPEED = 5000
+const JUMP_STRENGTH = 500
 const TERMINAL_VELOCITY = 20
 const MAX_WALK_SPEED = 20
 
 # Stores all the potential states that the player can be in, will be useful later to simplify complicated tasks.
 enum states {
 	GROUNDED,
-	FALLING
+	FALLING,
+	FLYING
 }
 
 var current_state = states.FALLING
 
 # Relative directions on the current planet.
-var relative_up = Vector2.UP
+var relative_up = Vector2.UP	
 var relative_down = Vector2.DOWN
 var relative_right = Vector2.RIGHT
 var relative_left = Vector2.LEFT
+var rel_up_last_frame = relative_up
+
+var lateral_movement = 0.0
+var gravitational_pull = 0.0
 
 func _physics_process(delta):
 	# Relative player direction vectors.
@@ -37,33 +43,44 @@ func _physics_process(delta):
 	match current_state:
 		
 		states.GROUNDED:
+			velocity = velocity.rotated(velocity.angle_to(planet.position))
+			
 			move_grounded(delta)
-			handle_gravity(delta)
+			
+			velocity = relative_down * gravitational_pull + relative_left * lateral_movement
+			
+			if Input.is_action_just_pressed("jump"):
+				velocity += relative_up * JUMP_STRENGTH
 			
 			if not is_grounded():
 				current_state = states.FALLING
 
 		states.FALLING:
 			handle_gravity(delta)
-			
+			velocity = relative_down * gravitational_pull + relative_left * lateral_movement
 			if is_grounded():
 				current_state = states.GROUNDED
 	
-	velocity = relative_down * 100
-	
+		states.FLYING: 
+			move_floating(delta)
 	move_and_slide()
 
 # Manipulates velocity according to gravitational formula.
 func handle_gravity(delta):
-	pass
+	gravitational_pull = ((667 * MASS * planet.MASS ) / position.distance_squared_to(planet.position)) * delta
+
+# Manipulates velocity according t
+func move_floating(delta):
+	velocity += Input.get_action_strength("move_left") * relative_left + Input.get_action_strength("move_right") * relative_right 
+
 # Manipulates velocity according to the player's lateral input (left, right).
 func move_grounded(delta):
-	pass
+	lateral_movement = Input.get_axis("move_left", "move_right") * SPEED * delta
 
 # Checks if the player is grounded using the floor collision raycast.
 func is_grounded():
 	return $RayDown.is_colliding()
-
+	
 # Clamps a vector at a given maximum length (will be useful when handling movement).
 func clamp_vector(vector, maximum_length):
 	if vector.length() > maximum_length:
